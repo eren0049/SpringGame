e25c4b7662fdc81269758c92704eefaaf7d8431b
eren0049 <eren.baysal1@gmail.com>
2023-02-09 11:06:06 +0100

    first commit

---
+++ src/main/java/com/in28minutes/learnspringframework/AppGamingJava.java
@@ -0,0 +1,19 @@
+package com.in28minutes.learnspringframework;
+
+import com.in28minutes.learnspringframework.game.GameRunner;
+import com.in28minutes.learnspringframework.game.MarioGame;
+
+public class AppGamingJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		var marioGame = new MarioGame();
+		var gameRunner= new GameRunner(marioGame);
+		gameRunner.run();
+		
+		
+		
+	}
+
+}
