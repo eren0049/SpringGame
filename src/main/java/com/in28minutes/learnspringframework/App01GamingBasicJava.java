ec3e014097a8c874bbd7dc8424a13d1d45a10fd7
eren0049 <eren.baysal1@gmail.com>
2023-02-10 12:27:18 +0100

    *Beans added

--- src/main/java/com/in28minutes/learnspringframework/App01GamingBasicJava.java
+++ src/main/java/com/in28minutes/learnspringframework/App01GamingBasicJava.java
@@ -5,16 +5,18 @@ import com.in28minutes.learnspringframework.game.MarioGame;
 import com.in28minutes.learnspringframework.game.PackmanGame;
 import com.in28minutes.learnspringframework.game.SuperContraGame;
 
-public class AppGamingJava {
+public class App01GamingBasicJava {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		
+		// Object
 		var game = new PackmanGame();
 		// var game = new MarioGame(); 
 		//var game = new SuperContraGame();
-		var gameRunner= new GameRunner(game);
+		
+		// Object & Wiring of dependencies
+		var gameRunner= new GameRunner(game); // can be called injection as well
 		gameRunner.run();
 		
 		
